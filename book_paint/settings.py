972ccb01486996ddd7c9467958c5834b605a937f
bruere_s <sebastien.bruere@epitech.eu>
2016-11-16 16:24:09 +0100

    OK

--- book_paint/settings.py
+++ book_paint/settings.py
@@ -119,7 +119,5 @@ DJANGO_SETTINGS_MODULE = 'django_app.settings'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
