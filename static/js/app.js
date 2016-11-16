23d584f20b08a6aa34a609ecb58dfd3c5c9a5823
bruere_s <sebastien.bruere@epitech.eu>
2016-11-16 15:36:05 +0100

    OK

--- static/js/app.js
+++ static/js/app.js
@@ -1,53 +1,84 @@
-$(document).ready(function() {
-  $("#button-d-menu").click(function() {
-    $("#d-menu").slideToggle( "slow", function() {
-    });
-    $("#header").mouseleave(function() {
-      ("#d-menu").slideToggle( "slow", function() {
+  var url = new URL(window.location.href);
+  var page = url.pathname.substring(1, url.pathname.length - 1);
+
+  $(document).ready(function() {
+
+    $("#button-d-menu").click(function() {
+      $("#d-menu").slideToggle( "fast", function() {
+      });
+      $("#header").mouseleave(function() {
+        ("#d-menu").slideToggle( "fast", function() {
+        });
       });
     });
-  });
-
-  $('#block-header-home').click( function(){
-    $('#block-header-home').addClass( "border-bottom");
-    $('#panel-home').show();
-    $('#panel-contact').hide();
-    $('#block-header-contact').removeClass( "border-bottom");
-    $('#panel-about').hide();
-    $('#block-header-about').removeClass( "border-bottom");
-    $('#panel-gallery').hide();
-    $('#block-header-gallery').removeClass( "border-bottom");
 
   });
-  $('#block-header-about').click( function(){
-    $('#block-header-about').addClass( "border-bottom");
-    $('#panel-about').show();
-    $('#panel-contact').hide();
-    $('#block-header-contact').removeClass( "border-bottom");
-    $('#panel-home').hide();
-    $('#block-header-home').removeClass( "border-bottom");
-    $('#panel-gallery').hide();
-    $('#block-header-gallery').removeClass( "border-bottom");
-  });
-  $('#block-header-gallery').click( function(){
-    $('#block-header-gallery').addClass( "border-bottom");
-    $('#panel-gallery').show();
-    $('#panel-contact').hide();
-    $('#block-header-contact').removeClass( "border-bottom");
-    $('#panel-about').hide();
-    $('#block-header-about').removeClass( "border-bottom");
-    $('#panel-home').hide();
-    $('#block-header-home').removeClass( "border-bottom");
-  });
-  $('#block-header-contact').click( function(){
-    $('#block-header-contact').addClass( "border-bottom");
-    $('#panel-contact').show();
-    $('#panel-about').hide();
-    $('#block-header-about').removeClass( "border-bottom");
-    $('#panel-home').hide();
-    $('#block-header-home').removeClass( "border-bottom");
-    $('#panel-gallery').hide();
-    $('#block-header-gallery').removeClass( "border-bottom");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    $('#block-header-home').click( function(){
+      $( "#panel-home" ).toggle('slide');
+      moveBorderHeader(0);
+      changeSlide(0);
+    });
+
+    $('#block-header-about').click( function(){
+      moveBorderHeader(1);
+      changeSlide(1);
+    });
+    $('#block-header-gallery').click( function(){
+      moveBorderHeader(2);
+      changeSlide(2);
+    });
+    $('#block-header-contact').click( function(){
+      moveBorderHeader(3);
+      changeSlide(3);
+    });
+
   });
 
-});
+  function moveBorderHeader(nb) {
+    var i = 0;
+    $('.block-header').each(function () {
+      if (i != nb) {
+        $(this).removeClass( "border-bottom");
+      }
+      else
+        $(this).addClass( "border-bottom");
+      i++;
+    });
+  }
+
+  function changeSlide(nb) {
+    var i = 0;
+    $('.slide').each(function () {
+      if (i != nb)
+        $(this).hide();
+      else
+        $(this).show();
+      i++;
+    });*/
+
+
+
